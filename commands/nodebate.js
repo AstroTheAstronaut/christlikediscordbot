4f6f5fdc67d39b68a56e08ff0f62298ad744dcea
MatteoS <matt.soho.17@gmail.com>
2022-06-11 13:24:52 +0300

    fixed again messagembed

--- commands/nodebate.js
+++ commands/nodebate.js
@@ -7,7 +7,7 @@ module.exports = {
     testOnly: false, // Only register a slash command for the testing guilds
     
     callback: ({ channel }) => {
-      const embed = new MessageEmbed
+      const embedNoDebate = new MessageEmbed()
         .setTitle("Please don't debate!")
         //.setDescription(`Please do not debate in this channel! Feel free to take this interesting conversation to a channel meant for debating, such as <#841255395598663730>.`)
         .setDescription(`Please keep all debates in <#841255395598663730>! Refusal to do so may lead to a warning!`)
