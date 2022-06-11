7bfb4314ec94174e82f0802d42b19f75c6b68cae
MatteoS <matt.soho.17@gmail.com>
2022-06-11 13:20:12 +0300

    fixed message embed

--- commands/nodebate.js
+++ commands/nodebate.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     category: 'General commands',
     description: 'Sends a nodebate message', // Required for slash commands
@@ -5,7 +7,7 @@ module.exports = {
     testOnly: false, // Only register a slash command for the testing guilds
     
     callback: ({ channel }) => {
-      const embed = new MessageEmbed()
+      const embed = new MessageEmbed
         .setTitle("Please don't debate!")
         //.setDescription(`Please do not debate in this channel! Feel free to take this interesting conversation to a channel meant for debating, such as <#841255395598663730>.`)
         .setDescription(`Please keep all debates in <#841255395598663730>! Refusal to do so may lead to a warning!`)
