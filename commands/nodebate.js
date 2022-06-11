50cd8a3f901f7a33121b6d74bcd2cb5ab3f412c1
MatteoS <matt.soho.17@gmail.com>
2022-06-11 13:18:58 +0300

    fixedf

--- commands/nodebate.js
+++ commands/nodebate.js
@@ -9,11 +9,7 @@ module.exports = {
         .setTitle("Please don't debate!")
         //.setDescription(`Please do not debate in this channel! Feel free to take this interesting conversation to a channel meant for debating, such as <#841255395598663730>.`)
         .setDescription(`Please keep all debates in <#841255395598663730>! Refusal to do so may lead to a warning!`)
-<<<<<<< HEAD
         .setFooter({text:`Requested by: ${message.author.tag} • Made with ❤️ by Astro for CHRISTLIKE`})
-=======
-        .setFooter({text:"Made with ❤️ by Astro for CHRISTLIKE!"})
->>>>>>> 639bc0cd207778ee236d51223685ca5ecf3cb023
         .setColor('#BCCEF1')
         channel.send({ embeds: [embed] });
     },
