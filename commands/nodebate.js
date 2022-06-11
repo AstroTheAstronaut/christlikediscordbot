fe52c5ad17515fa132767796a588c674209936f2
MatteoS <matt.soho.17@gmail.com>
2022-06-11 15:10:50 +0300

    Work pls

--- commands/nodebate.js
+++ commands/nodebate.js
@@ -7,12 +7,18 @@ module.exports = {
     testOnly: false, // Only register a slash command for the testing guilds
     
     callback: ({ channel,message }) => {
-      const embedNoDebate = new MessageEmbed()
+      /* const embedNoDebate = new MessageEmbed()
         .setTitle("Please don't debate!")
         //.setDescription(`Please do not debate in this channel! Feel free to take this interesting conversation to a channel meant for debating, such as <#841255395598663730>.`)
         .setDescription(`Please keep all debates in <#841255395598663730>! Refusal to do so may lead to a warning!`)
         .setFooter({text:`Made with ❤️ by Astro for CHRISTLIKE`})
         .setColor('#BCCEF1')
-        channel.send({ embeds: [embedNoDebate] });
+        channel.send({ embeds: [embedNoDebate] }); */
+        const noDebateEmbed = new MessageEmbed()
+          .setTitle("Please don't debate here!")
+          .setDescription(`Please keep all debates in <#841255395598663730>! Refusal to do so may lead to a warning!`)
+          .setFoother({text:"Made with ❤️ by Astro for CHRISTLIKE"})
+          .setColor('#BCEF1')
+          channel.send({embeds:[noDebateEmbed]});
     },
   }
