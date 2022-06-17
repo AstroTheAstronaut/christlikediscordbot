c7f711b2c9efdd747668929f0113349301530e87
MatteoS <matt.soho.17@gmail.com>
2022-06-17 23:06:26 +0300

    Added new command

---
+++ commands/commands.js
@@ -0,0 +1,31 @@
+module.exports = {
+  category: 'General Commands',
+  description: 'Lists all the commands I have!', // Required for slash commands
+
+  slash: 'both', // Create both a slash and legacy command
+  testOnly: false, // Only register a slash command for the testing guilds
+
+  callback: ({ channel, interaction }) => {
+    const commandsEmbed = {
+      color: '#bcef1',
+      title: "IHere are all the commands I currently have:",
+      fields: (
+        { name: 'commands', value: 'Lists all the commands.' },
+        { name: 'confine', value: 'Gives the Confined Member role.' },
+        { name: 'unconfine', value: 'Removes the Confined Member role.' },
+        { name: 'nodebate', value: 'Sends an embed that says to not debate.' },
+        { name: 'perms', value: 'Gives the Perms role' },
+        { name: 'verify', value: 'Gives the Verified role' },
+        { name: 'ping', value: 'Replies with Pong!' },
+        { name: 'role', value: 'Gives, removes or checks if the user has a role' },
+        { name: 'rule', value: 'Shows a rule you specify' },
+        { name: 'status', value: 'Changes the status of the bot' }
+      ),
+      footer: {
+        text: 'Made with ❤️ by Astro for CHRISTLIKE'
+      },
+
+    };
+    channel.send({ embeds: [debateEmbed] });
+  },
+}
