5219e09367ee8973a98c955b688d48f440bfffeb
MatteoS <matt.soho.17@gmail.com>
2022-06-17 23:12:21 +0300

    Fixed commands command

--- commands/commands.js
+++ commands/commands.js
@@ -12,16 +12,16 @@ module.exports = {
       title: "Here are all the commands I currently have:",
       description:'Commands:',
       fields: (
-        { name: 'commands', value: 'Lists all the commands.' },
-        { name: 'confine', value: 'Gives the Confined Member role.' },
-        { name: 'unconfine', value: 'Removes the Confined Member role.' },
-        { name: 'nodebate', value: 'Sends an embed that says to not debate.' },
-        { name: 'perms', value: 'Gives the Perms role' },
-        { name: 'verify', value: 'Gives the Verified role' },
-        { name: 'ping', value: 'Replies with Pong!' },
-        { name: 'role', value: 'Gives, removes or checks if the user has a role' },
-        { name: 'rule', value: 'Shows a rule you specify' },
-        { name: 'status', value: 'Changes the status of the bot' }
+        { name: 'commands', value: 'Lists all the commands.', inline:true },
+        { name: 'confine', value: 'Gives the Confined Member role.', inline:true },
+        { name: 'unconfine', value: 'Removes the Confined Member role.', inline:true  },
+        { name: 'nodebate', value: 'Sends an embed that says to not debate.', inline:true  },
+        { name: 'perms', value: 'Gives the Perms role', inline:true  },
+        { name: 'verify', value: 'Gives the Verified role', inline:true  },
+        { name: 'ping', value: 'Replies with Pong!', inline:true  },
+        { name: 'role', value: 'Gives, removes or checks if the user has a role', inline:true  },
+        { name: 'rule', value: 'Shows a rule you specify', inline:true  },
+        { name: 'status', value: 'Changes the status of the bot', inline:true  }
       ),
       footer: {
         text: 'Made with ❤️ by Astro for CHRISTLIKE'
