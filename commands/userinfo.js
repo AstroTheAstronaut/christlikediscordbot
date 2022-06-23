4c81e8e0e4e595e9aa07a417486912b23947016b
MatteoS <matt.soho.17@gmail.com>
2022-06-23 21:48:06 +0300

    Added new command

---
+++ commands/userinfo.js
@@ -0,0 +1,43 @@
+const moment = require('moment')
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+    category: 'Moderation command',
+    description: 'Shows information about a user', // Required for slash commands
+    
+    slash: 'both', // Create both a slash and legacy command
+    cooldown:'5s',
+    testOnly: false, // Only register a slash command for the testing guilds
+    
+    callback: ({ message, interaction }) => {
+        const target = message.mentions.users.first() || message.author()
+        const Member = message.guild.members.cache.get(target.id)
+
+        const responseEmbed = {
+            color: '#bcef1',
+            description: `${target.username}'s info:`,
+            footer:{
+                text:'Made with ❤️ by Astro for CHRISTLIKE'
+            },
+            fields: [ 
+                {
+                    name:'User ID:',
+                    value: `${target.id}`,
+                    inline:false
+                },
+                {
+                    name:'Roles:',
+                    value:`${Member.roles.cache.map(r=>r).join(' ').replace("@everyone", " ")}`
+                },
+                {
+                    name:'Server age:',
+                    value:`${moment(Member.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\\n**-** ${moment(Member.joinedAt).startOf('day').fromNow()}`
+                },
+                {
+                    name:'Account age:',
+                    value:`${moment(Member.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\\n**-** ${moment(Member.createdAt).startOf('day').fromNow()}`
+                }
+            ]
+        }
+        message.reply({embeds:[responseEmbed]})
+    },
+  }
