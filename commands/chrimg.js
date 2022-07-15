691ea5ab64ed904f139a4345109270cb4f8e3532
MatteoS <matt.soho.17@gmail.com>
2022-07-15 16:00:02 +0300

    fixed

--- commands/chrimg.js
+++ commands/chrimg.js
@@ -5,7 +5,7 @@ module.exports = {
     slash: 'both', // Create both a slash and legacy command
     testOnly: false, // Only register a slash command for the testing guilds
     cooldown:'5s',
-    callback: ({ message, args,guild }) => {
+    callback: ({ channel, args,guild }) => {
       
       const image = "https://cdn.discordapp.com/attachments/840673462498099260/997443338707943564/image0.jpg"
       const imageEmbed = {
