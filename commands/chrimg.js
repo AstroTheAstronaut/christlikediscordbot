90a26a76078689c47a0c126a553b543018d70723
MatteoS <matt.soho.17@gmail.com>
2022-07-15 15:55:40 +0300

    Added new weird command

---
+++ commands/chrimg.js
@@ -0,0 +1,21 @@
+module.exports = {
+    category: 'General commands',
+    description: 'Sends a very specific image', // Required for slash commands
+    permissions:['MUTE_MEMBERS'],
+    slash: 'both', // Create both a slash and legacy command
+    testOnly: false, // Only register a slash command for the testing guilds
+    cooldown:'5s',
+    callback: ({ message, args,guild }) => {
+      
+      const image = "https://cdn.discordapp.com/attachments/840673462498099260/997443338707943564/image0.jpg"
+      const imageEmbed = {
+        color:'#bcef1',
+        image: {
+          url:image
+        },
+        footer:{
+          text:'Made with ❤️ by Astro for CHRISTLIKE'
+        },
+    };
+    },
+  }
